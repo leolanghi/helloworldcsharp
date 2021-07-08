c87babd7b2f3f0c537ac30f340397aad5cb942d7
Leonardo Langhi <leonardo@langhi.com.br>
2021-07-07 21:10:31 -0300

    Adicionamos os dizeres Bom dia!

--- Cap08_GitHub/Program.cs
+++ Cap08_GitHub/Program.cs
@@ -7,6 +7,7 @@ namespace Cap08_GitHub
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom dia!!!");
             Console.ReadLine();
         }
     }
