f68ab87ab1704e8f88c8e363c9c870962b099566
Leonardo Langhi <leonardo@langhi.com.br>
2021-07-07 21:00:31 -0300

    Criacao da leitura de console

---
+++ Cap08_GitHub/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Cap08_GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
